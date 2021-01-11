47864a3c781975a7d2cd22dce52576c1b3352274
Debian <debian@debian10>
2021-01-11 14:46:06 +0000

    change default location

--- src/store/state.ts
+++ src/store/state.ts
@@ -2,12 +2,12 @@ import { MapState } from '../types';
 
 export const InitialState: MapState = {
     map: {
-        center: [100.55532, 13.98083],
+        center: [-1.7030681187784467, 48.11947479723537],
         pitch: 60,
         maxPitch: 60,
         bearing: 51,
         zoom: 17,
-        country:'thailand',
+        country:'france',
         building:''
     }
 }
