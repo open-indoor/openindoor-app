ca9dfc18e60ab678eb22624fd1abc97f9f4a1961
Clement IGONET <clement.igonet@openindoor.io>
2021-01-19 04:54:56 +0000

    fix APP_URL for prod

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ let API_URL = "";
 // local:      apX-local.openindoor.io
 if (process.env.NODE_ENV === "production") {
   // NOTE if API_URL and APP_URL not set in environment then use static url
-  // API_URL = "https://" + process.env.API_DOMAIN_NAME
+  API_URL = "https://api.openindoor.io";
   // APP_URL = "https://" + process.env.APP_DOMAIN_NAME
 } else if (process.env.NODE_ENV === "stage") {
   API_URL = "https://api-sandbox.openindoor.io";
