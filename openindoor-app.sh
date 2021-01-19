c3604bbb04c5fa9f46db3c39f40474ec987db14e
Clement IGONET <clement.igonet@openindoor.io>
2021-01-19 09:39:32 +0000

    trigger build on any branch

--- openindoor-app.sh
+++ openindoor-app.sh
@@ -1,7 +1,13 @@
 #/bin/bash
+### set domain names ###
 for f in `find /data/www -type f -name "*.js"`; do
   sed -i 's/\(api\.openindoor\.io\)/'$API_DOMAIN_NAME'/g' $f
   sed -i 's/\(app\.openindoor\.io\)/'$APP_DOMAIN_NAME'/g' $f
 done
 
+### inject analytics code ###
+sed 's/<!-- analytics -->/\$ANALYTICS/' /data/www/index.html > /data/www/index.html_ANALYTICS
+cat /data/www/index.html_ANALYTICS | envsubst | tee /data/www/index.html
+
+### Start web server ###
 (caddy fmt /etc/caddy/Caddyfile & caddy run --watch --config /etc/caddy/Caddyfile)
